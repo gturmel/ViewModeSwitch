13ba71ef9f960e4a91ae6962b87da276aca4dc41
Greg Turmel <gregturmel@gmail.com>
2016-02-14 22:01:19 -0500

    add switch view MVP

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,10 +7,15 @@ var gridIcon = document.getElementById("grid");
 
 var listIcon = document.getElementById("list");
 
-var liFormat = document.getElementsByTagName("li");
+var titleOfProduct = document.getElementsByTagName("h3")
 
-var css = document.getElementsByClassName("block");
+var price = document.getElementsByClassName("price");
 
+var description = document.getElementsByClassName("description");
+
+var cartButton = document.getElementsByClassName("cartButton");
+
+var listTag = document.getElementsByTagName("li");
 
 // This changes the header icons to a different color when clicked
 
@@ -29,3 +34,59 @@ listIcon.addEventListener("click", function (){
 });
 
 // Here's where the list-block style changes happen
+
+gridIcon.addEventListener("click", function(){
+for (num = 0; num <= price.length; num++) {
+   price[num].style.display = ""
+   }
+});
+
+listIcon.addEventListener("click", function(){
+for (num = 0; num <= price.length; num++) {
+   price[num].style.display = "inline";
+
+   }
+});
+
+gridIcon.addEventListener("click", function(){
+for (num = 0; num <= titleOfProduct.length; num++) {
+   titleOfProduct[num].style.display = ""
+   }
+});
+
+listIcon.addEventListener("click", function(){
+for (num = 0; num <= titleOfProduct.length; num++) {
+   titleOfProduct[num].style.display = "inline";
+
+   }
+});
+
+gridIcon.addEventListener("click", function(){
+for (num = 0; num <= description.length; num++) {
+   description[num].style.display = ""
+   }
+});
+
+listIcon.addEventListener("click", function(){
+for (num = 0; num <= description.length; num++) {
+   description[num].style.display = "inline";
+
+   }
+});
+
+gridIcon.addEventListener("click", function(){
+for (num = 0; num <= cartButton.length; num++) {
+   cartButton[num].style.display = ""
+   }
+});
+
+listIcon.addEventListener("click", function(){
+for (num = 0; num <= cartButton.length; num++) {
+   cartButton[num].style.display = "inline";
+
+   }
+});
+
+gridIcon.addEventListener("click", function(){
+   
+})
